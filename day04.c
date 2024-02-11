9397f5083defbd2c8b01069df10ff86fefd4e988
SNaNdii <singhnandita73@gmail.com>
2024-02-11 11:39:48 +0530

    PRoblem solve

--- day04.c
+++ day04.c
@@ -5,13 +5,12 @@ int main(){
     printf("Enter age here : \n");
     scanf("%d", age);
 
-    // if(age >= 18){
-    //     printf("Valid age for diving licence");
-    // }
-    // else{
-    //     printf("InValid age for diving licence");
-    // }
-
+    if(age >= 18){
+        printf("Valid age for diving licence");
+    }
+    else{
+        printf("InValid age for diving licence");
+    }
 
 
 
@@ -19,6 +18,28 @@ int main(){
 
     age >= 18 ?  printf("Valid age for diving licence") : printf("InValid age for diving licence");   
 
+
+
+    //Switch case----------------------------------
+    int day;
+    printf("Enter day number (1-7) : \n");
+    scanf("%d", &day);
+    switch(day){
+        case 1: printf("Monday");
+        break;
+        case 2: printf("Tuesday");
+        break;
+        case 3: printf("Wednesday");
+        break;
+        case 4: printf("Thursday");
+        break;
+        case 5: printf("Friday");
+        break;
+        case 6: printf("Saturday");
+        break;
+        case 7: printf("Sunday");
+        default : printf("Number not valid");
+    }
     return 0;
 }
 
---
+++ day04_problem.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    char c;
+    printf("Enter your character here : \n");
+    scanf("%c", &c);
+
+    if(c >= 'a' && c <='z'){
+        printf("Lower Case \n");
+    }
+    else if(c >= 'A' && c <= 'Z'){
+        printf("Upper case \n");
+    }
+    else{
+        printf("Not an english character");
+    }
+
+    return 0;
+}

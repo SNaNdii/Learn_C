ee8e8b6119f98da204a3415b4464713338f9d3a9
SNaNdii <singhnandita73@gmail.com>
2024-02-11 11:21:19 +0530

    Conditional Statement Complete

--- day04.c
+++ day04.c
@@ -1,1 +1,25 @@
-// 
+// COnditional Statements-----------
+#include<stdio.h>
+int main(){
+    int age;
+    printf("Enter age here : \n");
+    scanf("%d", age);
+
+    // if(age >= 18){
+    //     printf("Valid age for diving licence");
+    // }
+    // else{
+    //     printf("InValid age for diving licence");
+    // }
+
+
+
+
+    //Ternary Operator-----------------------------------
+
+    age >= 18 ?  printf("Valid age for diving licence") : printf("InValid age for diving licence");   
+
+    return 0;
+}
+
+

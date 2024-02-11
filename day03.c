baf16fea151029fab2ecbd142fe40c2705824875
SNaNdii <singhnandita73@gmail.com>
2024-02-11 10:23:15 +0530

    All Opearators Complete

--- day03.c
+++ day03.c
@@ -1,7 +1,9 @@
-// Arithmatic operator----------------
 #include<stdio.h>
 #include<math.h>
+
+
 int main(){
+    // Arithmatic operator----------------
     int a,b,c;
     a=b=c=3;
     printf("Sum : %d \n", a+b+c);
@@ -12,5 +14,18 @@ int main(){
 
     float power = pow(a,b);
     printf("a to the power b : %.2f \n", power);
+    
+
+
+    // Relational operator----------------
+    printf("%d \n", 4==3); // false returns 0
+    printf("%d \n", 4 > 3); // true returns 1
+    printf("%d \n", 4 != 3);
+
+
+    // Logical operator----------------
+    // && and operator , || or operator , ! not operator
+    printf("Result of AND operator is %d \n", 4 != 3 && 4 < 6);
+    printf("Result of OR operator is  %d \n", !(4 == 3 || 4 < 6));
     return 0;
 }

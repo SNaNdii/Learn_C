28b6b397c284d9bfa9351e2f2d9711e8e38b665c
SNaNdii <singhnandita73@gmail.com>
2024-02-13 19:02:23 +0530

    Call by reference and value completed

--- day09.c
+++ day09.c
@@ -1,6 +1,25 @@
-#include<std.io>
+#include<stdio.h>
 
+void square(int n);
+void sum(int* n);
 int main(){
-    printf("Hello");
+    int num = 10;
+    square(num);
+    printf("Number = %d \n", num);
+
+    sum(&num);
+    printf("Number = %d \n", num);
     return 0;
+}
+
+// Call  By  Value---------------
+void square(int n){
+    n = n*n;
+    printf("Square = %d \n", n);
+}
+
+// Call  By  Reference------------
+void sum(int* n){
+    *n = (*n) + (*n);
+    printf("Sum = %d \n", *n);
 }
